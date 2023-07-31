71ca0c1f1f23bbbb0e82bebf18be7f38462a1ee7
gtsofa <jlsnyule@yahoo.com>
2023-07-31 20:17:28 +0300

    Make the HTTPClient a protocol

--- QuizMobileApp/QuizMobileAppTests/RemoteQuestionLoaderTests.swift
+++ QuizMobileApp/QuizMobileAppTests/RemoteQuestionLoaderTests.swift
@@ -8,19 +8,44 @@
 import XCTest
 
 class RemoteQuestionLoader {
+    let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "http://a-url.com")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class RemoteQuestionLoaderTests: XCTestCase {
     func test_init_doesNotLoadDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteQuestionLoader()
+        let client = HTTPClientSpy()
+        _ = RemoteQuestionLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteQuestionLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
