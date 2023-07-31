d8435e7279f27293c3e212e4618a4eab3991574b
gtsofa <jlsnyule@yahoo.com>
2023-07-31 13:06:11 +0300

    Create the QuestionItem model

---
+++ QuizMobileApp/QuizMobileApp/QuestionItem.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionItem.swift
+//  QuizMobileApp
+//
+//  Created by Julius on 31/07/2023.
+//
+
+import Foundation
+
+struct QuestionItem {
+    let question: String
+    let answer: [String]
+}
