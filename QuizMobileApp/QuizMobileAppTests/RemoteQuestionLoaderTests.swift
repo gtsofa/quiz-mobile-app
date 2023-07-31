8e58ed9cb1c756c9c959926152a1a50e34c1cce2
gtsofa <jlsnyule@yahoo.com>
2023-07-31 23:37:54 +0300

    Refactor tests to remove duplication

--- QuizMobileApp/QuizMobileAppTests/RemoteQuestionLoaderTests.swift
+++ QuizMobileApp/QuizMobileAppTests/RemoteQuestionLoaderTests.swift
@@ -25,31 +25,37 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 final class RemoteQuestionLoaderTests: XCTestCase {
     func test_init_doesNotLoadDataFromURL() {
-        let url = URL(string: "http://a-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteQuestionLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
         let url = URL(string: "http://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteQuestionLoader(url: url, client: client)
+        
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertNotNil(client.requestedURL)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "http://a-url.com")!) -> (sut: RemoteQuestionLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteQuestionLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 
 }
