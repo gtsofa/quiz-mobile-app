68dbbcd08ed6bbd246e2930703df29bf44da3af8
gtsofa <jlsnyule@yahoo.com>
2023-08-01 14:09:24 +0300

    Remove default value for completion block (moved it to the test)

--- QuizMobileApp/QuizMobileApp/Question API/RemoteQuestionLoader.swift
+++ QuizMobileApp/QuizMobileApp/Question API/RemoteQuestionLoader.swift
@@ -24,7 +24,7 @@ public class RemoteQuestionLoader {
         case connectivitiy
     }
     
-    public func load(completion: @escaping (Error) -> Void = { _ in } ) {
+    public func load(completion: @escaping (Error) -> Void ) {
         client.get(from: url) { error in
             completion(.connectivitiy)
         }
