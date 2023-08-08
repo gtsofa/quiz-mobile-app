7c7086b3324dc0c609c9208618f18c66de5cf053
gtsofa <jlsnyule@yahoo.com>
2023-08-08 19:35:13 +0300

    Make URLSessionHTTPClient conform to the HTTPClient protocol

--- QuizMobileApp/QuizMobileAppTests/Question API/URLSessionHTTPClientTests.swift
+++ QuizMobileApp/QuizMobileAppTests/Question API/URLSessionHTTPClientTests.swift
@@ -87,7 +87,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeak(sut, file: file, line: line)
         return sut
